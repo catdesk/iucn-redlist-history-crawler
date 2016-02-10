2ee208c5350f8fc6bb50c1394e4340899f993232
catdesk <catdesk@tuta.io>
2016-02-10 23:23:07 +0100

    Loop through species list and collect all species_data in a tuple

--- crawl.py
+++ crawl.py
@@ -2,38 +2,60 @@ import mechanicalsoup
 
 species = ["Ailuropoda melanoleuca", "Ardea insignis", "Chthonicola sagittatus"]
 base_url = "http://www.iucnredlist.org"
-
-print(base_url)
+species_data = []
 
 browser = mechanicalsoup.Browser()
 page = browser.get(base_url + "/search")
-search_form = page.soup.select("form")[0]
-search_form.select("#quickSearchText")[0]['value'] = species
 
-search_results = browser.submit(search_form, page.url)
+def get_species_data(page, species):
+    search_form = page.soup.select("form")[0]
+    search_form.select("#quickSearchText")[0]['value'] = species
 
-species_listing = search_results.soup.find("span", class_="sciname", string=species[2])
-species_listing_link = species_listing.previous_element.attrs['href']
-species_page = browser.get(base_url + species_listing_link)
+    search_results = browser.submit(search_form, page.url)
 
-# Need to process synonyms out of html. for entry in synonyms_unprocessed, concatenate what's in spans? do i need to keep "subspecies" (see Ardea insignis)
-synonyms_unprocessed = species_page.soup.find(string="Synonym(s):").find_previous("tr").find_all("div")
+    species_listing = search_results.soup.find("span", class_="sciname", string=species)
+    species_listing_link = species_listing.previous_element.attrs['href']
+    species_page = browser.get(base_url + species_listing_link)
+    
+    print(species_listing)
+    print(species_listing_link)
+    print(species_page)
 
-# Process IUCN Red List Assessments
-assessment_table = species_page.soup.find(string="Previously published Red List assessments:").find_previous("tr").find_all("tr")
-assessments = []
+    # Need to process synonyms out of html. for entry in synonyms_unprocessed, concatenate what's in spans? do i need to keep "subspecies" (see Ardea insignis)
+    synonyms_unprocessed = ''
+    if species_page.soup.find(string="Synonym(s):") != None:
+      synonyms_unprocessed = species_page.soup.find(string="Synonym(s):").find_previous("tr").find_all("div")
 
-for row in assessment_table:
-    cells = row.find_all("td")
-    year = cells[0].string
-    level = cells[2].next_element.string.strip().split()[-1].strip('()')
-    assessments.append((year, level))
+    synonyms = []
+    
+    for synonym in synonyms_unprocessed:
+        #not finished here....
+        synonyms.append(str(synonym).strip("</div>").strip()) # for just turning it all into a string 
+        print(synonyms)
+    
+    # Process IUCN Red List Assessments
+    assessment_table = species_page.soup.find(string="Previously published Red List assessments:").find_previous("tr").find_all("tr")
+    assessments = []
+    
+    for row in assessment_table:
+        cells = row.find_all("td")
+        year = cells[0].string
+        level = cells[2].next_element.string.strip().split()[-1].strip('()')
+        assessments.append((year, level))
 
-print(assessments)
+    species_data.append((species, synonyms, assessments))
+
+for specie in species:
+    print("#####specie#####")
+    print(specie)
+    get_species_data(page, specie)
+    
+
+
+print("######species_data#####")
 print("##########1##########")
-print(species_listing)
+print(species_data)
 print("##########2##########")
-print(synonyms_unprocessed)
 print("##########3##########")
 print("##########4##########")
 
