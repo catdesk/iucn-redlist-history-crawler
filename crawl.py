98d77f0a77a240b9da80d4472cfd28c82b7b674a
catdesk <catdesk@tuta.io>
2016-02-10 17:45:15 +0100

    Grab synonyms

--- crawl.py
+++ crawl.py
@@ -1,6 +1,6 @@
 import mechanicalsoup
 
-species = "Ailuropoda melanoleuca"
+species = ["Ailuropoda melanoleuca", "Ardea insignis", "Chthonicola sagittatus"]
 base_url = "http://www.iucnredlist.org"
 
 print(base_url)
@@ -12,16 +12,14 @@ search_form.select("#quickSearchText")[0]['value'] = species
 
 search_results = browser.submit(search_form, page.url)
 
-species_listing = search_results.soup.find("span", class_="sciname", string=species)
+species_listing = search_results.soup.find("span", class_="sciname", string=species[2])
 species_listing_link = species_listing.previous_element.attrs['href']
 species_page = browser.get(base_url + species_listing_link)
+synonyms_unprocessed = species_page.soup.find(string="Synonym(s):").find_previous("tr").find_all("div")
 
 print("##########1##########")
 print(species_listing)
 print("##########2##########")
-print(species_listing_link)
 print("##########3##########")
-print(species_page.soup)
 print("##########4##########")
-#print(species_listing)
 
