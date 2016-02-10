a05e8530e0a859c9f10bc74b6b7fb6070944c703
catdesk <catdesk@tuta.io>
2016-02-10 19:19:20 +0100

    Grab and process assessments

--- crawl.py
+++ crawl.py
@@ -15,11 +15,25 @@ search_results = browser.submit(search_form, page.url)
 species_listing = search_results.soup.find("span", class_="sciname", string=species[2])
 species_listing_link = species_listing.previous_element.attrs['href']
 species_page = browser.get(base_url + species_listing_link)
+
+# Need to process synonyms out of html. for entry in synonyms_unprocessed, concatenate what's in spans? do i need to keep "subspecies" (see Ardea insignis)
 synonyms_unprocessed = species_page.soup.find(string="Synonym(s):").find_previous("tr").find_all("div")
 
+# Process IUCN Red List Assessments
+assessment_table = species_page.soup.find(string="Previously published Red List assessments:").find_previous("tr").find_all("tr")
+assessments = []
+
+for row in assessment_table:
+    cells = row.find_all("td")
+    year = cells[0].string
+    level = cells[2].next_element.string.strip().split()[-1].strip('()')
+    assessments.append((year, level))
+
+print(assessments)
 print("##########1##########")
 print(species_listing)
 print("##########2##########")
+print(synonyms_unprocessed)
 print("##########3##########")
 print("##########4##########")
 
