321cc9d10a8ed10a39b60555b11840ac3b314ad3
catdesk <catdesk@tuta.io>
2016-02-19 17:38:02 +0100

    Making processing more accurate hopefully

--- process_csv.py
+++ process_csv.py
@@ -33,7 +33,6 @@ for line in listfile_unprocessed:
         assessment_list.append([assessment_year, assessment_val])
     else:
         species_data.update({sci_name: [[assessment_year, assessment_val]]})
-    species_data[sci_name].sort()
     print(species_data[sci_name])
 
 def write_results(sci_name, year, assessed_val):
@@ -46,25 +45,26 @@ def write_results(sci_name, year, assessed_val):
         print(row_list)
         outputter.writerow(row_list)
 
+# Read it backwards from 2015 to 1998 instead of dealing with sketchy sorting, still hasn't been tested
 for sci_name, assessment_list in species_data.items():
     print(sci_name)
     print(assessment_list)
     last_assessed_val = 'NA'
-    last_assessed_year = 1965
+    last_assessed_year = 2015
     assessment_table = {}
     for assessment in assessment_list:
-        year = 1965
+        year = 2015
         print(assessment)
         assessment_year = int(assessment[0])
         assessed_val = assessment[1]
-        while year < 2016:
-            if assessment_year == year:
+        while year > 1997:
+            if assessment_year <= year:
                 last_assessed_val = assessed_val
                 last_assessed_year = assessment_year
-            if year >= last_assessed_year:
+            if year <= last_assessed_year:
                 assessment_table[year] = last_assessed_val
                 print(year, last_assessed_val)
-            year += 1
+            year -= 1
     for row in assessment_table.items():
         if row[0] > 1997:
             write_results(sci_name, row[0], row[1])
