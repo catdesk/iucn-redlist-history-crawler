b051b22c198210f25027a4f6aba73f534ea69572
catdesk <catdesk@tuta.io>
2016-02-20 01:37:59 +0100

    Reading backwards was a bad idea, just needed the .reverse in the right place

--- process_csv.py
+++ process_csv.py
@@ -20,12 +20,20 @@ for line in listfile_unprocessed:
     sci_name = line[0]
     assessment_year = line[-2]
     assessment_val = line[-1]
-    if assessment_val == 'V':
+    if assessment_val == 'V' or assessment_val == 'Vulnerable':
         assessment_val = 'VU'
-    elif assessment_val == 'CR      (Possibly Extinct)':
+    elif 'Critically Endangered' in assessment_val and '(Possibly Extinct)' in assessment_val:
         assessment_val = 'CR(PE)'
-    elif assessment_val == 'E':
+    elif 'Critically Endangered' in assessment_val:
+        assessment_val = 'CR'
+    elif assessment_val == 'E' or assessment_val == 'Endangered':
         assessment_val = 'EN'
+    elif assessment_val == 'Near Threatened':
+        assessment_val = 'NT'
+    elif assessment_val == 'Data Deficient':
+        assessment_val = 'DD'
+    elif assessment_val == 'Least Concern':
+        assessment_val = 'LC'
     assessment_values.add(assessment_val)
     print(sci_name)
     if sci_name in species_data:
@@ -33,7 +41,6 @@ for line in listfile_unprocessed:
         assessment_list.append([assessment_year, assessment_val])
     else:
         species_data.update({sci_name: [[assessment_year, assessment_val]]})
-    print(species_data[sci_name])
 
 def write_results(sci_name, year, assessed_val):
     with open(out_file, 'a', newline='') as csvfile:
@@ -47,24 +54,24 @@ def write_results(sci_name, year, assessed_val):
 
 # Read it backwards from 2015 to 1998 instead of dealing with sketchy sorting, still hasn't been tested
 for sci_name, assessment_list in species_data.items():
+    assessment_list.reverse()
     print(sci_name)
-    print(assessment_list)
     last_assessed_val = 'NA'
-    last_assessed_year = 2015
+    last_assessed_year = 1965
     assessment_table = {}
     for assessment in assessment_list:
-        year = 2015
-        print(assessment)
+        year = 1965
         assessment_year = int(assessment[0])
         assessed_val = assessment[1]
-        while year > 1997:
-            if assessment_year <= year:
+        while year < 2016:
+            if assessment_year == year:
                 last_assessed_val = assessed_val
                 last_assessed_year = assessment_year
-            if year <= last_assessed_year:
+            if year >= last_assessed_year:
                 assessment_table[year] = last_assessed_val
                 print(year, last_assessed_val)
-            year -= 1
+            year += 1
+    print(assessment_list)
     for row in assessment_table.items():
         if row[0] > 1997:
             write_results(sci_name, row[0], row[1])
